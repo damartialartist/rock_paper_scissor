9bf209461b5fb49179352ea00ce6cdc78cbee57d
damartialartirst <jackychenglhf@gmail.com>
2024-04-15 14:48:16 -0700

    Completed game logic

--- game.js
+++ game.js
@@ -1,11 +1,19 @@
 function getComputerChoice() {
-    return Math.floor(Math.random() * 3)
+    return Math.floor(Math.random() * 3) + 1;
 }
 
+function decodeChoice(choice) {
+    if (choice === 1) {
+        return "Rock";
+    } else if (choice === 2) {
+        return "Paper";
+    } else {
+        return "Scissor"
+    }
+}
 
 function playRound(playerSelection, computerChoice) {
     playerSelection = playerSelection.toLowerCase();
-
     if (playerSelection === "rock") {
         playerSelection = 1;
     } else if (playerSelection === "paper") {
@@ -41,9 +49,15 @@ function playRound(playerSelection, computerChoice) {
     if((playerSelection == 3) && (computerChoice == 2)) {
         return "You Win";
     }
+
+    return "Game done"
 }
 
 let otherChoice = getComputerChoice();
-console.log(otherChoice)
-const playerSelection = "rock";
-playRound(playerSelection, otherChoice);
+let playerSelection = prompt("Enter Rock/Paper/Scissor");
+let outcome = playRound(playerSelection, otherChoice);
+
+otherChoice = decodeChoice(otherChoice);
+
+console.log(`Computer chose: ${otherChoice}, ${outcome}`)
+
